36f3a70c7e5004c9cdc5306b62d7b3c531b9501c
simplesolutiondev <simplesolution.devteam@gmail.com>
2021-03-02 00:25:42 +0700

    Spring Boot Web using Undertow Embedded Servlet Container
    Spring Boot Web using Undertow Embedded Servlet Container

---
+++ src/main/java/dev/simplesolution/undertow/SpringBootUndertowApplication.java
@@ -0,0 +1,13 @@
+package dev.simplesolution.undertow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUndertowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUndertowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/simplesolution/undertow/SpringBootUndertowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simplesolution.undertow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUndertowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
